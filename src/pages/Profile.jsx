864a55d64754a11d962f2d356cf7815efc0fba69
WarisTheDon <99509063+WarisTheDon@users.noreply.github.com>
2025-11-10 03:52:46 +0100

    login + register working

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import Card from "../components/Card";
 
 const Profile = ({ listings }) => {
+  const user = JSON.parse(localStorage.getItem("user")); // ← henter user fra browser
+
+  if (!user) {
+    return <p>Du er ikke logget inn</p>;
+  }
+
+  function logout() {
+    localStorage.removeItem("user");
+    window.location.href = "/"; // send tilbake til home
+  }
+
   return (
     <div className="profile">
       <h2>Min profil</h2>
+
+      <p><strong>Brukernavn:</strong> {user.name}</p>
+      <p><strong>Epost:</strong> {user.email}</p>
+
+      <button onClick={logout}>Logg ut</button>
+
+      <hr style={{margin: "20px 0"}} />
+
       <p>Dine annonser:</p>
       <div className="cards-container">
         {listings.length === 0 && <p>Du har ingen annonser enda.</p>}
