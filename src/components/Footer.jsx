0b864a76930577a19c7cd0bb0048497136c1e3fe
WarisTheDon <99509063+WarisTheDon@users.noreply.github.com>
2025-12-02 05:03:16 +0100

    liten update
    legger til mer context i notater / fjerner gamle elr unnødvendige notater

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styles/app.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 Kids Klesskap. All rights reserved.</p>
+      <p>© 2025 Kids Klesskap. Alle rettigheter reservert.</p>
     </footer>
   );
 };
